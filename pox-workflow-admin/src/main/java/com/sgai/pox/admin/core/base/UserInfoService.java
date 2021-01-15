0329b11076dab81d61eab9a7ce3a6bfc43ff2423
lrz0828 <16601106677@163.com>
2021-01-15 14:31:41 +0800

    去掉 security

--- pox-workflow-admin/src/main/java/com/sgai/pox/admin/core/base/UserInfoService.java
+++
@@ -1,8 +0,0 @@
-package com.sgai.pox.admin.core.base;
-
-
-import com.sgai.pox.engine.core.base.Result;
-
-public interface UserInfoService {
-    Result<UserInfo> info(String userId, String inner);
-}
